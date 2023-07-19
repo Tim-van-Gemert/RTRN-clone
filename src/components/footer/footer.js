665d9ec9e1f0d162407326a8c858987937c8f1e8
Tim van Gemert <82870317+Tim-van-Gemert@users.noreply.github.com>
2023-07-19 19:52:21 +0200

    made footer responsive

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -34,20 +34,20 @@ export default function Footer() {
 
 
     return ( 
-        <div className="w-site flex flex-col gap-[32px] pb-[80px]">
-            <div className="w-full grid grid-cols-4   pb-[80px] gap-[80px] pt-[208px] border-b-[1px] border-black ">
+        <div className=" w-full px-[16px] xl:px-[0px] xl:w-site flex flex-col gap-[32px] pb-[40px] xl:pb-[80px]">
+            <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4   pb-[40px] xl:pb-[80px]  gap-[60px] xl:gap-[80px] pt-[208px] border-b-[1px] border-black ">
                 {footerWidgets.map((widget) => {
                     return (
                         widget
                     )
                 })}
             </div>
-            <div className="flex flex-row justify-between ">
+            <div className="flex flex-col xl:flex-row gap-[48px] p-small xl:justify-between ">
                     <div>© 2023 RTRN Creative Agency</div>
-                <div className="flex flex-row gap-[48px]">
-                    <a>Algemene voorwaarden</a>
-                    <a>Cookies</a>
-                    <a>Privacy</a>
+                <div className="flex flex-col xl:flex-row gap-[48px]">
+                    <a className=" md:hidden lg:flex border-b-[1px] w-fit md:border-b-[0px] border-black">Algemene voorwaarden</a>
+                    <a className=" md:hidden lg:flex border-b-[1px] w-fit md:border-b-[0px] border-black">Cookies</a>
+                    <a className="  md:hidden lg:flex border-b-[1px] w-fit md:border-b-[0px] border-black">Privacy</a>
                 </div>
             </div>
         </div>
--- src/components/templates/button.js
+++ src/components/templates/button.js
@@ -1,5 +1,4 @@
 export default function Button (props) {
-    console.log(props.content)
     return (
         <div className=" h-fit flex flex-row gap-[16px] transition-all hover:cursor-pointer ">
             <a href={props.to} className={`${props.content == undefined? 'p-3' : 'p-5'} rounded-full transition-all flex flex-row hover:flex-row-reverse gap-[8px] border-[1px] ${ props.hoverColor == 'yellow' ? 'hover:bg-main hover:border-main' : 'hover:bg-white'}  ${ props.baseColor == 'yellow' ? 'bg-main' : 'bg-transparant'}  transition-all border-black`}>
