4ff775240bf7ecbc116295b4f4f9b40fb4c11d98
Tim van Gemert <82870317+Tim-van-Gemert@users.noreply.github.com>
2023-07-20 16:13:04 +0200

    commit to vercel

--- src/components/stepSection/singleStep.js
+++ src/components/stepSection/singleStep.js
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import Button from "../templates/button";
+import { useState, useEffect } from "react";
 
 export default function SingleStep(props) {
     const { ref, inView } = useInView({
@@ -11,12 +12,12 @@ export default function SingleStep(props) {
     return (
         <motion.div
             ref={ref}
-            className="w-full h-full group flex justify-between group py-[16px] xl:pl-[32px] border-b-[1px] xl:border-b-[0px] xl:border-l-[1px] border-black flex-col"
+            className="w-full group flex justify-between group py-[16px] xl:pl-[32px] border-b-[1px] xl:border-b-[0px] xl:border-l-[1px] border-black flex-col"
             initial={{ height: inView ? '0%' : '100%' }}
-            animate={{ height: inView ? '100%' : '0%' }}
+            animate={{ height: inView ? '100%' : '10%' }}
             transition={{ duration: 1, delay: props.delay }}
         >
-            <div className="flex flex-col gap-[32px]">
+            <div className="flex flex-col w-full gap-[32px] pb-[40px] md:pb-[0px]">
                 <h4 className="steps">{props.title}</h4>
                 <Button
                     key={props.title + "Button"}
@@ -26,7 +27,7 @@ export default function SingleStep(props) {
                     content={props.button}
                 />
             </div>
-            <p className={`opacity-0 group-hover:opacity-100 transition-all p-small w-[336px]`}>
+            <p className={` hidden md:block opacity-0 group-hover:opacity-100 transition-all p-small w-[336px]`}>
                 {props.desc}
             </p>
         </motion.div>
